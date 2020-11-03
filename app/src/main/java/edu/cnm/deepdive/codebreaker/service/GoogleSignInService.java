c4a71be4d7927c0d7fdf1416b3a771be18091d00
Shayan Golafshani <sgolafshani@cnm.edu>
2020-11-03 09:20:06 -0700

    Started back-end integration.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInService.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.codebreaker.service;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
+import android.util.Log;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import edu.cnm.deepdive.codebreaker.BuildConfig;
 
 public class GoogleSignInService {
 
@@ -25,8 +27,8 @@ public class GoogleSignInService {
         .requestEmail()
         .requestId()
         .requestProfile()
+        .requestIdToken(BuildConfig.CLIENT_ID)
         .build();
-//        .requestIdToken(BuildConfig.CLIENT_ID)
 // this is a bearer token, the back end takes this bearer token and passes it to google, and asks
     // google if the bearer token matches what it requests.
 
@@ -37,7 +39,7 @@ public class GoogleSignInService {
     GoogleSignInService.context = context;
   }
 
-  public  static GoogleSignInService getInstance(){
+  public static GoogleSignInService getInstance() {
     return InstanceHolder.INSTANCE;
   }
 
@@ -46,23 +48,23 @@ public class GoogleSignInService {
   }
 
   //return a task to silently sign the user in if their token is expired!
-  public Task<GoogleSignInAccount> refresh(){
+  public Task<GoogleSignInAccount> refresh() {
     return client.silentSignIn()
-        .addOnSuccessListener((account -> this.account  = account));
+        .addOnSuccessListener(this::setAccount);
   }
 
-  public void startSignin (Activity activity, int requestCode){
+  public void startSignin(Activity activity, int requestCode) {
     account = null;
     Intent intent = client.getSignInIntent();
     activity.startActivityForResult(intent, requestCode);
   }
 
   // we created this method to check for a completed sign-in.
-  public Task<GoogleSignInAccount> completeSignIn(Intent data){
+  public Task<GoogleSignInAccount> completeSignIn(Intent data) {
     Task<GoogleSignInAccount> task = null;
     try {
       task = GoogleSignIn.getSignedInAccountFromIntent(data);
-      account = task.getResult(ApiException.class);
+      setAccount(task.getResult(ApiException.class));
     } catch (ApiException e) {
       //exception will be pass automatically to onFailureListener
     }
@@ -70,13 +72,22 @@ public class GoogleSignInService {
   }
 
   //regardless of whether the account signs out or not.... sign us out.
-  public Task<Void> signOut(){
+  public Task<Void> signOut() {
     return client.signOut()
-        .addOnCompleteListener((ignored) -> account = null);
+        .addOnCompleteListener((ignored) -> setAccount(null));
   }
+
+  private void setAccount(GoogleSignInAccount account) {
+    this.account = account;
+    if (account != null) {
+      //noinspection ConstantConditions
+      Log.d(getClass().getSimpleName(), account.getIdToken());
+    }
+  }
+
   private static class InstanceHolder {
 
-  private static final GoogleSignInService INSTANCE = new GoogleSignInService();
+    private static final GoogleSignInService INSTANCE = new GoogleSignInService();
 
   }
 
