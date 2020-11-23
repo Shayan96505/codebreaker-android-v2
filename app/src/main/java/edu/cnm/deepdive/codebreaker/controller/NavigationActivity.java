146571d725b7778da1b65ba62e8fee224dd5c709
Shayan Golafshani <sgolafshani@cnm.edu>
2020-11-23 16:17:39 -0700

    added MatchFragment an the navigation associated with it.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/NavigationActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/NavigationActivity.java
@@ -40,7 +40,7 @@ public class NavigationActivity extends AppCompatActivity {
 
   private void setupNavigation() {
     appBarConfig = new Builder(
-        R.id.navigation_game, R.id.navigation_summary, R.id.navigation_settings)
+        R.id.navigation_game, R.id.navigation_summary, R.id.navigation_settings, R.id.navigation_match)
         .setOpenableLayout(binding.drawerLayout)
         .build();
     //noinspection ConstantConditions
